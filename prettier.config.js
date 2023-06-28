65003f40c808601c5d98845e918b2ea3b88b0b33
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-27 20:11:18 -0700

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  printWidth: 80,
+  endOfLine: 'lf',
+}
