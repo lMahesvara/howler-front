f76053f790cbecdc9fd79009010bfa2b0541ded8
German <german_gomez1205@hotmail.com>
2023-06-30 00:11:14 -0700

    Update Icons.jsx

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,7 +1,12 @@
-import { MessageCircle, Heart, Repeat2 } from 'lucide-react'
+import { MessageCircle, Heart, Repeat2, Home, Bell, BellDot, Mail, User2 } from 'lucide-react'
 
 export const Icons = {
   MessageCircle: MessageCircle,
   Heart: Heart,
   Repeat2: Repeat2,
+  Home: Home,
+  Bell: Bell,
+  BellDot: BellDot,
+  Messages: Mail,
+  Profile: User2
 }
