05e96e52a6de8ba35aeb23d75eab5bed3fc027da
Erick Bernal Amparano <erick082016@gmail.com>
2023-06-30 17:30:21 -0700

    feat: create Profile Header component

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,8 +1,17 @@
-import { MessageCircle, Heart, Repeat2, MoreHorizontal } from 'lucide-react'
+import {
+  MessageCircle,
+  Heart,
+  Repeat2,
+  MoreHorizontal,
+  ArrowLeft,
+  CalendarDays,
+} from 'lucide-react'
 
 export const Icons = {
   MessageCircle: MessageCircle,
   Heart: Heart,
   Repeat2: Repeat2,
   MoreHorizontal: MoreHorizontal,
+  ArrowLeft: ArrowLeft,
+  CalendarDays: CalendarDays,
 }
