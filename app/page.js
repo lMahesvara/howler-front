d2c821897dd4817ca2acd3a44b2b86fb9fc215a8
Erick Bernal Amparano <erick082016@gmail.com>
2023-07-02 20:28:54 -0700

    feat: add page layouts

--- app/page.js
+++ app/page.js
@@ -1,18 +1,29 @@
+import BackButton from '@/components/BackButton'
+import PageLayout from '@/components/PageLayout'
 import Post from '@/components/Post'
 import ProfileHeader from '@/components/ProfileHeader'
 
-export default function Home() {
+export default function Profile() {
   return (
-    <div className='grow shrink'>
-      <main className=' flex flex-col items-center border border-[#2f3336] w-full min-h-screen overflow-x-hidden grow shrink max-w-[600px]'>
-        <section className='max-w-[700px]'>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </section>
-      </main>
-    </div>
+    <>
+      <PageLayout.Container>
+        <PageLayout.Header>
+          <BackButton />
+          <div className='flex flex-col items-start justify-center flex-grow'>
+            <PageLayout.HeaderTitle className='text-[#e7e9ea] whitespace-nowrap font-bold text-[17px] leading-5 sm:text-xl'>
+              Kura
+            </PageLayout.HeaderTitle>
+            <h3 className='text-[#71767b] whitespace-nowrap font-normal text-[13px] leading-4'>
+              570 Howls
+            </h3>
+          </div>
+        </PageLayout.Header>
+        <ProfileHeader />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PageLayout.Container>
+    </>
   )
 }
