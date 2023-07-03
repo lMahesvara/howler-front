65fc5582fc0fba4dfb798559c0b9762b45c44717
German <german_gomez1205@hotmail.com>
2023-07-03 16:21:53 -0700

    Feat. RegisterForm.jsx (Component)

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 const RegisterForm = () => {
   return (
-    <section className='py-16'>
-      <h1 className='text-4xl font-bold'>
+    <section className='py-16 shrink grow s:pb-8'>
+      <h1 className='text-4xl font-bold s:hidden'>
         WHAT IS <br /> HAPPENING NOW...
       </h1>
 
-      <p className='text-2xl font-semibold mt-32'>Join Howler today.</p>
-      <div className='flex flex-col items-start justify-center w-[55%] gap-5 mt-8'>
+      <p className='mt-32 text-2xl font-semibold s:mt-0'>Join Howler today.</p>
+      <div className='flex flex-col items-center justify-center w-[55%] gap-5 mt-14'>
         <button
           type='submit'
-          className='h-12 px-6 transition duration-300 rounded-md group bg-white hover:bg-gray-700 hover:text-white text-black font-semibold w-full cursor-pointer'>
+          className='w-full h-12 px-6 font-semibold text-black transition duration-300 bg-white rounded-md cursor-pointer group hover:bg-gray-700 hover:text-white overflow-ellipsis'>
           Sign up
         </button>
 
         <button
-          className='h-12 px-6 transition duration-300 rounded-md group bg-white hover:bg-gray-700 hover:text-white w-full'
+          className='w-full h-12 px-6 transition duration-300 bg-white rounded-md group hover:bg-gray-700 hover:text-white'
           // onClick={() => signIn('google')}
         >
           <div className='flex flex-row items-center justify-center space-x-4'>
-            <span className='text-sm font-semibold tracking-wide transition duration-300 w-max group-hover:text-white sm:text-base text-black'>
+            <span className='text-sm font-semibold tracking-wide text-black transition duration-300 w-max group-hover:text-white sm:text-base'>
               Sign in with Google
             </span>
             <img
@@ -32,7 +32,7 @@ const RegisterForm = () => {
         </button>
 
         <div>
-          <p className='text-xs italic w-full'>
+          <p className='w-full text-xs italic'>
             By signing up, you agree to the{' '}
             <a
               className='text-xs italic underline hover:text-blue-600'
