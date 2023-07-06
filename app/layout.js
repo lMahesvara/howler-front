51f1ece48a293ce317c8e3f22ab6ef55673cfdb0
German <german_gomez1205@hotmail.com>
2023-07-05 21:59:29 -0700

    Create Toast.jsx

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import PageLayout from '@/components/PageLayout'
+import { Toaster } from "@/components/Toaster"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className + ` flex min-h-screen`}>
         {/* <Navbar /> */}
         {children}
+        <Toaster />
       </body>
     </html>
   )
