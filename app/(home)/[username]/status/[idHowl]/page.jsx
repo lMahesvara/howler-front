05060a24797c2d8ad26ca90efcdcf20e82e92071
Erick Bernal Amparano <erick082016@gmail.com>
2023-07-07 12:56:08 -0700

    feat: remove session

--- app/(home)/[username]/status/[idHowl]/page.jsx
+++ app/(home)/[username]/status/[idHowl]/page.jsx
@@ -5,17 +5,12 @@ import ExtendedPost from '@/components/ExtendedPost'
 import ListOfComments from '@/components/ListOfComments'
 import PageLayout from '@/components/PageLayout'
 import { useHowl } from '@/hooks/useHowl'
-import { useSession } from 'next-auth/react'
 
 const Page = ({ params }) => {
-  const { data, status } = useSession()
-  const user = data?.user
-
   const { idHowl } = params
   const { howl, isLoading } = useHowl(idHowl)
 
   if (isLoading) return null
-  console.log(user)
 
   return (
     <PageLayout.Container>
@@ -27,13 +22,8 @@ const Page = ({ params }) => {
           </PageLayout.HeaderTitle>
         </div>
       </PageLayout.Header>
-      <ExtendedPost howl={howl} id={user?._id} />
-      <CreatePost
-        idHowl={idHowl}
-        label={'Howl your reply!'}
-        user={user}
-        status={status}
-      />
+      <ExtendedPost howl={howl} />
+      <CreatePost idHowl={idHowl} label={'Howl your reply!'} />
       <ListOfComments replies={howl.replies} />
     </PageLayout.Container>
   )
--- app/(home)/home/page.js
+++ app/(home)/home/page.js
@@ -5,22 +5,13 @@ import PageLayout from '@/components/PageLayout'
 import Post from '@/components/Post'
 import { getHome } from '@/services/api'
 import useSWR from 'swr'
-import { useSession } from 'next-auth/react'
 
 export default function Home() {
-  const { data, status } = useSession()
-  const user = data?.user
-  const {
-    data: dataHowl,
-    error,
-    isLoading,
-  } = useSWR('/api/howls', () => getHome(), {
-    refreshInterval: 1000,
-  })
+  const { data, error, isLoading } = useSWR('/api/howls', () => getHome())
 
   useEffect(() => {
-    if (dataHowl) console.log(dataHowl)
-  }, [dataHowl])
+    if (data) console.log(data)
+  }, [data])
 
   if (isLoading) return <div>Loading...</div>
 
@@ -34,10 +25,10 @@ export default function Home() {
           </PageLayout.HeaderTitle>
         </div>
       </PageLayout.Header>
-      <CreatePost label={'What is happening?!'} status={status} user={user} />
+      <CreatePost label={'What is happening?!'} />
       <section className='w-full'>
-        {dataHowl?.map((howl, index) => (
-          <Post key={index} idHowl={howl._id} id={user?._id} />
+        {data?.map((howl, index) => (
+          <Post key={index} idHowl={howl._id} />
         ))}
       </section>
     </PageLayout.Container>
